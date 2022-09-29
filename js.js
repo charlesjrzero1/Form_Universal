a0c424102237cafa72077eea87bf9198881edb9e
charles j <charlesvieroliver@gmail.com>
2022-09-29 00:14:43 -0300

    first commit

---
+++ js.js
@@ -0,0 +1,12 @@
+function print(){
+    let nome =document.getElementById("Nome");
+    let user =document.getElementById("Username");
+    let email=document.getElementById("Email");
+    let label=document.getElementById("Select");
+    let textarea=document.getElementById("Textarea");
+    let teste= alert("Cadastro concluido. " )
+    console.log(nome.value);
+    console.log(user.value);
+    console.log(email.value);
+    console.log(label.value);
+    console.log(textarea.value);}
